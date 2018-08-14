8d4950838aa7cb16010de5518e524fd68274c87b
Vlad Szabo - SINF 2016 <vlad.2010@yahoo.com>
2018-08-15 00:00:17 +0300

    Update shapes

--- SmallTetris/tetris.py
+++ SmallTetris/tetris.py
@@ -1,6 +1,5 @@
 from pygame import display as d, KEYUP as K, event as v, draw as p, time as t
-from random import randint as f
-from json import *
+from random import randint as f, choice as k
 
 """
     s -> shapes
@@ -22,88 +21,17 @@ from json import *
     For now, we separate variables for clarity but we keep short names to check if it fits
 
                464 -> 111010000
-
-    1) T-form: 111          001         000         100
-               010  -90->   011 -90->   010 -90->   110
-               000          001         111         100
-
-               432
-    
-    2) O-form: 110          011         000         000
-               110  -90->   011 -90->   011 -90->   110
-               000          000         011         110
-    
-    
-               294
-    
-    3) L-form: 100          111         011         000
-               100  -90->   100 -90->   001 -90->   001
-               110          000         001         111
-               
-               150
-               
-    4) J-form: 010          100         011         000
-               010  -90->   111 -90->   010 -90->   111
-               110          000         010         001
-               
-               487
-
-    5) C-form: 111          
-               100  -90->   
-               111          
-               
-               146
-               
-    6) I-form: 010
-               010
-               010
-               
-               240
-
-    7) S-form: 011
-               110
-               000
-               
-               408
-               
-    8) Z-form: 110
-               011
-               000
-    
-    
+                              
+    Have the base.
+    Make a lambda that rotates it once and expands it by 20x20
 """
 
-print([[[*map(int, f"{bin(j)[2:]}")][i:i+3]for i in range(0, 9, 3)]for j in [464, 432, 294, 150, 487, 146, 240, 408]])
-shape = [[1, 1, 1], [1, 0, 0], [1, 1, 1]]
-rotated_shape_90 = [[shape[j][i] for j in range(3)][::-1] for i in range(3)]
-rotated_shape_180 = [[rotated_shape_90[j][i] for j in range(3)][::-1] for i in range(3)]
-rotated_shape_270 = [[rotated_shape_180[j][i] for j in range(3)][::-1] for i in range(3)]
-
-for i in shape:
-    print("".join(str(j) for j in i))
-print()
-for i in rotated_shape_90:
-    print("".join(str(j) for j in i))
-print()
-for i in rotated_shape_180:
-    print("".join(str(j) for j in i))
-print()
-for i in rotated_shape_270:
-    print("".join(str(j) for j in i))
-
-
-s = [[[i+[0]*20 for i in r]+[[0]*20]*20 for r in s] for s in loads(open("s").read())]
-m = [[f(0, 0) for _ in range(10)] for i in range(20)]
-m[f(0, 19)] = [1]*10
-m[f(0, 19)] = [1]*10
-g = d.set_mode((200, 400))
-c, r = s[2], 0
-x, y = 3, 0
-b = (lambda q: ~all(q)+2,)
-
+b = (lambda q: ~all(q)+2, lambda q: [[q[j][i] for j in range(3)][::-1]+q[i][3:] for i in range(3)]+q[3:])
+s = [[[*map(int, format(int(i), '03b'))]+[0]*20 for i in str(j)]+[[0]*20]*20 for j in [66, 222, 36, 63, 446, 226, 72]]
+m, g, c, x, y = [[f(0, 0) for _ in range(10)] for i in range(20)], d.set_mode((200, 400)), k(s), 4, 0
 while True:
     # handle movement and rotation
-    x, r = [[{1: (x+1, r), 2: (x-1, r), 0: (x, r+1)}.get(e.key-274) for e in v.get() if e.type == K]+[(x, r)]][0][0]
+    x, c = [[{1: (x+1, c), 2: (x-1, c), 0: (x, b[1](c))}.get(e.key-274) for e in v.get() if e.type == K]+[(x, c)]][0][0]
 
     # draw map
     [[p.rect(g, [0, 255][m[i][j]], (j*20, i*20, 20, 20)) for j in range(10)] for i in range(20)]
@@ -112,11 +40,10 @@ while True:
     d.update(), t.Clock().tick(30)
 
     # shape moves down constantly
-    m, y = [[[m[i][j] > 0, -1][c[r % len(s)][i-int(y)][j-x]] for j in range(10)] for i in range(20)], [y+.1, y][y > 16]
+    m, y = [[[m[i][j] > 0, -1][c[i-int(y)][j-x]] for j in range(10)] for i in range(20)], [y+.1, y][y > 16]
 
     # move map down when full lines are present
     m = (20-len([*filter(b[0], m)]))*[[0]*10]+[*filter(b[0], m)]
-
 """
 Steps:
     Shape should be represented with -1 or 2 on the map
